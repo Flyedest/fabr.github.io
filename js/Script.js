b586ae9a5f2dfea65083848f3f535859425d7ee5
Plinio Fabrycio <pliniofabrycio94@gmail.com>
2018-04-13 06:58:11 -0300

    10 porcento concluido

---
+++ js/Script.js
@@ -0,0 +1,23 @@
+function cortina(){
+    
+    var target = document.getElementById('cortina');
+    var mainContent = document.getElementById('mainContent');
+    
+    if (target.classList.contains('active')){
+        
+        target.classList.remove('active');
+        target.classList.add('deactive');
+        
+        mainContent.classList.remove('esconder');
+        mainContent.classList.add('mostrar');
+        
+    } else {
+         target.classList.remove('deactive');
+         target.classList.add('active');
+        
+         mainContent.classList.remove('mostrar');
+         mainContent.classList.add('esconder');
+    }
+}
+
+
